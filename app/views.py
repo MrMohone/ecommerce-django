904969e6c18c5e734b23327674d8b3d012b4fbbc
MrMohone <mo37085825@gmail.com>
2025-03-16 03:34:42 +0300

    display dynamic data from db

--- app/views.py
+++ app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
-from urllib import request
+from django.db.models import Count
 from django.views import View
+from .models import Product
 
 def home(request):
     return render(request, "app/home.html")
 
 class CategoryView(View):
     def get(self, request, val):
+        #Fetch from db
+        product = Product.objects.filter(category=val)#fetch all matched values like title, price...
+        title = Product.objects.filter(category=val).values('title')#fetch only title
         
         return render(request, 'app/category.html', locals())
