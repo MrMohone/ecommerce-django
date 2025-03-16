440baab2f7ae7b35065d97e57279b0cde2ee38b5
MrMohone <mo37085825@gmail.com>
2025-03-16 21:44:39 +0300

    about page added successfully

--- app/urls.py
+++ app/urls.py
@@ -6,7 +6,9 @@ from . import views
 
 urlpatterns = [
     path("", views.home ),
-    path("category/<slug:val>", views.CategoryView.as_view(), name='category'),
+    path("about", views.about, name='about'),
+    path("contact", views.contact, name='contact'),
+    path("category/<slug:val>", views.CategoryView.as_view(), name='category'),#as.view() => for class
     path("category-title/<val>", views.CategoryTitle.as_view(), name="product-title"),
     path("product-detail/<int:pk>", views.ProductDetail.as_view(), name='product-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#for displaying images
